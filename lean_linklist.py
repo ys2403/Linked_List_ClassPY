9e82f4543303cb1e1b94a812066d79f8994406a7
ys2403 <yashdshah2403@gmail.com>
2024-06-15 06:32:01 -0700

    traversal of linkedlist

--- lean_linklist.py
+++ lean_linklist.py
@@ -7,15 +7,15 @@ class linkedlist:
 	def __init__(self) :
  		self.head = None
 
-# 	def trav_ll (self):				# Traversal of LL
+	def trav_ll (self):				# Traversal of LL
 	
-# 		if self.head is None:
-# 			print (" LL is empty")
-# 		else:
-# 			n = self.head
-# 			while n is not None:
-# 				print(n.data,"--->", end = " ")
-# 				n = n.ref
+		if self.head is None:
+			print (" LL is empty")
+		else:
+			n = self.head
+			while n is not None:
+				print(n.data,"--->", end = " ")
+				n = n.ref
 		
 # 	def add_beg(self, data):		# add node at start
 # 		new_node = node(data)
